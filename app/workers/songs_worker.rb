a67d162c5f0b4d716f5fc51735798ee91396fa2d
NStephenson <nickstephenson90@gmail.com>
2016-04-14 18:56:42 -0400

    made all tests pass

---
+++ app/workers/songs_worker.rb
@@ -0,0 +1,11 @@
+class SongsWorker
+  include Sidekiq::Worker 
+
+  def peform(song_file)
+    CSV.foreach(params["file"].path, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end
+  end
+
+
+end
