0c2398134c123f03b763cc6d07e06bbb3e2425d0
johanesteves <jsesteves91@gmail.com>
2017-12-14 11:44:32 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -23,10 +23,9 @@ class SongsController < ApplicationController
     end
   end
 
+
   def upload
-    CSV.foreach(params["file"].path, headers: true) do |song|
-      Song.create(title: song[0], artist_name: song[1])
-    end
+    SongsWorker.perform_async(params[:file].path)
     redirect_to songs_path
   end
 
