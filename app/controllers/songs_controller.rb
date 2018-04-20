d438734e4936cc0939c07b631da366b89cd28d5b
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-04-20 10:08:25 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,6 +34,11 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+  def upload
+    SongsWorker.perform_async(params[:file].path)
+    redirect_to songs_path
+  end
+
   def update
     @song = Song.find(params[:id])
 
@@ -59,4 +64,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
