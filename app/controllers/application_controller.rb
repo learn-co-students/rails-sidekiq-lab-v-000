67084db5f8edeafde3386f79ec34fb0c8f6682e5
James Lotruglio <jimlotruglio@yahoo.com>
2018-12-04 22:49:53 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+
+  def upload()
+    SongsWorker.perform_async(params[:file].path)
+    redirect_to songs_path
+  end 
 end
