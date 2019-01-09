aa9847c017544b474e6b6c247e525520543ea395
micahshute <micah.shute@gmail.com>
2019-01-09 11:34:25 -0600

    Done.

---
+++ app/workers/songs_worker.rb
@@ -0,0 +1,10 @@
+class SongsWorker 
+    include Sidekiq::Worker
+    require 'csv'
+
+    def perform(path)
+        CSV.foreach(path, headers: true) do |song|
+            Song.create(title: song[0], artist_name: song[1])
+        end
+    end
+end
