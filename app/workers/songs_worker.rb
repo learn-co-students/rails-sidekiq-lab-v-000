8eeabcdacb6b047dca39994f138dd31cf5393fa0
Justine B <justinegayton@gmail.com>
2019-03-18 21:56:03 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,7 +4,6 @@ class SongsWorker
   include Sidekiq::Worker
 
   def perform(file)
-    binding.pry
     CSV.foreach(file, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
     end
