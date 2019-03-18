1f5df5958578b4e4792aa452579e790f2cb06bda
Justine B <justinegayton@gmail.com>
2019-03-18 21:48:12 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -5,7 +5,7 @@ class SongsWorker
 
   def perform(file)
     CSV.foreach(file, headers: true) do |song|
-      Song.create(title: song[0])
+      Song.create(title: song[0], artist_name: song[1])
     end
   end
 end
