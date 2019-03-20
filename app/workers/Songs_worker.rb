ac9c84ae9dc8441296e161186152465eb534685c
Steve Albonico <salbonico@gmail.com>
2019-03-20 16:50:31 -0700

    Done.

--- app/workers/Songs_worker.rb
+++ app/workers/Songs_worker.rb
@@ -4,5 +4,6 @@ class SongsWorker
   def perform(songs_file)
     CSV.foreach(params["file"].path, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
+    end
   end
 end
