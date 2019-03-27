b110c07d60ee0969cf861a155019d2acca2ea2f1
Tahseen Siddiqui <tahseensiddiqui@Tahseens-MacBook-Pro.local>
2019-03-26 18:07:47 -0700

    Done.

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,7 +4,6 @@ class SongsWorker
 
   def perform(song_file)
     CSV.foreach(song_file, headers: true) do |song|
-      binding.pry
       Song.create(title: song[0], artist_name: song[1])
     end
   end
