3df8e3bf13cc137aa79f8c85528ef4ec738875c7
Joeycho <joeyadamcho@gmail.com>
2019-09-22 06:27:05 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, only: [:index, :show, :edit, :new]
+    resources :artists, only: [:index, :show]
+    post 'songs/upload', to: 'songs#upload'
+
 end
