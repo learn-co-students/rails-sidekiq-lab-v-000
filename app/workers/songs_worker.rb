931ac2d4f8133abbab7549012921d4078eefc92d
RonSala <ron.sala@gmail.com>
2020-03-22 18:19:40 -0400

    Done.

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -3,7 +3,8 @@ class SongsWorker
   include SideKiq::Worker
 
   def perform(songs_file)
-    CSV.foreach(params[:file].path, headers: true) do |s|
+    # CSV.foreach(params[:file].path, headers: true) do |s|
+    CSV.foreach(songs_file, headers: true)
       Song.create(title: s[0], artist_name: s[1])
     end
   end
